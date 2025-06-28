123929a3f22b152b7b708a21c8423c4872ee8396
abhay <abhayakg123@gmail.com>
2025-06-28 16:20:44 +0530

    feat: clerk auth with react-hook-form components

--- schema/signUpSchema.ts
+++ schema/signUpSchema.ts
@@ -1,6 +1,6 @@
 import {z} from 'zod';
 
-export const signUpSchmea = z.object({
+export const signUpSchema = z.object({
     email: z.string().email("Invalid email address").min(1, "Email is required"),
     password: z.string().min(8, "Password must be at least 8 characters long"),
     confirmPassword: z.string().min(1, "Please confirm your password"),
